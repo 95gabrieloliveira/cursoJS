d1960047a9b6cb217b617bc637a26573f83aa8b7
Gabriel Oliveira <95gabrieloliveira@gmail.com>
2022-08-21 22:37:29 -0300

    Create index.js

---
+++ Variáveis com Let/index.js
@@ -0,0 +1,15 @@
+let nome = "Gabriel";// Declarou a Variável
+
+// Não podemos criar variáveis com palavras reservadas pelo JavaScript Ex. Let, if else
+// Variáveis precisam ter nomes significativos e explicativos.
+// Não pode conter numeros no inicio e espaço ou traços.
+// Utilizamos camelCase para mais de uma palavra.
+// JavaScript é Case-sensititive por isso diferencia letras minusculas e maiusculas
+// Não podemos redeclarar variáveis com let
+
+
+console.log(nome, 'nasceu em 1984.');
+console.log('Em 2000', nome , 'conheceu Maria');
+console.log(nome, 'casou-se com Maria em 2012');
+console.log('Maria teve 1 Filho com', nome, 'em 2015');
+console.log('O filho de', nome, 'se chama Eduardo');
