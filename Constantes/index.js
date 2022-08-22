0642497325095372b7050fc1efeb2fcc6f651c10
Gabriel Oliveira <95gabrieloliveira@gmail.com>
2022-08-21 23:02:53 -0300

    Constantes

---
+++ Constantes/index.js
@@ -0,0 +1,24 @@
+// Não podemos criar constante com palavras reservadas pelo JavaScript Ex. Let, if else
+// Constantes precisam ter nomes significativos e explicativos.
+// Não pode conter numeros no inicio e espaço ou traços.
+// Utilizamos camelCase para mais de uma palavra.
+// JavaScript é Case-sensititive por isso diferencia letras minusculas e maiusculas
+// Não podemos redeclarar variáveis com let
+//Não podemos modificar o valor de uma constante por isso é necessário colocar valor na criação
+// Não utilize VAR, utilize CONST
+const nome = 'Gabriel';
+
+// typeof comando para saber o tipo do dados (String, Number etc) 
+
+console.log(nome);
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2
+
+console.log(resultadoDuplicado);
+
+
+
+
